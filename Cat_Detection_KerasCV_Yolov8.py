4b719fc2dc72e670177557b4c1e30706a2cb2a07
Yadisbel <yadisbel.martinez@gmail.com>
2024-09-22 22:32:07 -0400

    change extension

---
+++ Cat_Detection_KerasCV_Yolov8.py
@@ -0,0 +1,65 @@
+!pip install tensorflow keras-cv
+
+import tensorflow as tf
+from tensorflow import keras
+import keras_cv
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from google.colab import files
+
+# Load pre-trained YOLOv8 model
+model = keras_cv.models.YOLOV8Detector.from_preset(
+    "yolo_v8_m_pascalvoc", bounding_box_format="xywh",
+)
+
+
+# Upload an image
+uploaded = files.upload()
+
+# Assuming the first uploaded file is the image
+image_path = list(uploaded.keys())[0]
+
+# Load the image using PIL
+image = Image.open(image_path)
+
+# Display the image
+image.show()
+
+# Resize the image
+image_resized = tf.image.resize(image, (640, 640))[None, ...]
+
+
+# Make predictions on the image
+predictions  = model.predict(image_resized)
+
+
+cat_label = 7
+
+# Create a plot to display the image with bounding boxes
+plt.figure(figsize=(8, 8))
+plt.imshow(image)
+
+# KerasCV returns a dictionary with predictions: boxes, scores, and labels
+boxes = predictions['boxes'][0]  # Bounding boxes
+scores = predictions['confidence'] [0] # Confidence scores
+labels = predictions['classes'][0] # Class labels
+
+cat_label = 7
+
+# Create a plot to display the image with bounding boxes
+plt.figure(figsize=(8, 8))
+plt.imshow(image)
+for i in range(len(labels)):
+      if labels[i] == cat_label and scores[i] > 0.5:  # Filter for cats and high-confidence scores
+         # Get the coordinates of the bounding box
+         x1, y1, w, h = boxes[i]         
+         # Draw a rectangle around the detected cat
+         plt.gca().add_patch(plt.Rectangle((int(x1), int(y1)), int(h), int(w),
+                                          edgecolor='red', facecolor='none', linewidth=2))
+         # Optionally, label the box with the score
+         plt.text(x1, y1, f"Cat: {scores[i]:.2f}", color='white', backgroundcolor='red')
+
+# Remove axis and display the plot
+plt.axis("off")
+plt.show()
